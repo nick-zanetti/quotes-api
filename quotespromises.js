e4d908e80f72a280b3d04289b9cb031e3b676f9e
Nick Zanetti <nzanetti44@gmail.com>
2020-05-09 13:16:51 -0400

    changed to a better api

--- quotespromises.js
+++ quotespromises.js
@@ -1,12 +1,13 @@
 const button = document.querySelector('#button')
+const quote = document.querySelector("#quote")
 
 const getQuote = () => {
-  return fetch('http://quotes.stormconsultancy.co.uk/random.json')
+  return fetch('https://type.fit/api/quotes')
   .then((response) => {
     return response.json()
     .then((parsedResponse) => {
-      const quoteText = document.querySelector('#quote');
-      quoteText.textContent = `${parsedResponse.author}: "${parsedResponse.quote}"`
+      const randQuote = parsedResponse[Math.floor(Math.random() * parsedResponse.length)]
+      randQuote.author ? quote.textContent = `${randQuote.author}: "${randQuote.text}"` : quote.textContent = `Unknown: "${randQuote.text}"`
     })
   })
 }
